d7103d32207816ee0517695d48723fec1906a3b1
Ashwin <ashwinhsacodes@gmail.com>
2020-07-08 19:13:32 +0530

    Discovery Server Registry with Ribbon Client Project Code.

---
+++ SpringCloud/2-EurekaDiscoveryWithRibbon/EmployeeConsumer/src/main/java/com/cloud/controller/ConsumerControllerClient.java
@@ -0,0 +1,46 @@
+package com.cloud.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+//@Controller
+public class ConsumerControllerClient {
+
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+
+	public void getEmployee() throws RestClientException, IOException {
+
+		ServiceInstance serviceInstance = loadBalancerClient.choose("employee-producer");
+
+		String baseUrl = serviceInstance.getUri().toString();
+
+		baseUrl = baseUrl + "/employee";
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = null;
+		try {
+			response = restTemplate.exchange(baseUrl, HttpMethod.GET, getHeaders(), String.class);
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+		System.out.println(response.getBody());
+	}
+
+	private static HttpEntity<?> getHeaders() throws IOException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		return new HttpEntity<>(headers);
+	}
+}
