9921782852666851d88804ba2d7e8668b5f32d77
Ashwin Kumar <ashwinhsacodes@gmail.com>
2024-06-20 20:34:16 +0530

    Adding Logger statements

--- PRIVATECLOUD/userservice/src/main/java/com/user/controller/DepartmentController.java
+++ PRIVATECLOUD/userservice/src/main/java/com/user/controller/DepartmentController.java
@@ -2,6 +2,8 @@ package com.user.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,17 +17,21 @@ import com.user.service.DepartmentService;
 @RestController
 @RequestMapping("department")
 public class DepartmentController {
+	
+	Logger logger = LoggerFactory.getLogger(DepartmentController.class);
 
 	@Autowired
 	DepartmentService departmentService;
 	
 	@PostMapping("addDepartment")
 	public Department addDepartment(@RequestBody Department department) {
+		logger.info("Department : " + department);
 		return departmentService.addDepartment(department);
 	}
 	
 	@GetMapping("listAllDepartments")
 	public List<Department> getAllDepartments(){
+		logger.info("getAllDepartments() method called");
 		return departmentService.getAllDepartments();
 	}
 }
--- PRIVATECLOUD/userservice/src/main/java/com/user/controller/UserController.java
+++ PRIVATECLOUD/userservice/src/main/java/com/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.user.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,32 +19,45 @@ import com.user.service.UserService;
 @RequestMapping("user")
 public class UserController {
 
+	Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	UserService userService;
 	
 	@GetMapping("/hello")
 	public String sayHelloMethod() {
+
+        // Logging various log level messages 
+        logger.trace("Log level: TRACE"); 
+        logger.info("Log level: INFO"); 
+        logger.debug("Log level: DEBUG"); 
+        logger.error("Log level: ERROR"); 
+        logger.warn("Log level: WARN"); 
+		
 		return "Hello from user service";
 	}
 	
 	@PostMapping("adduser")
 	public User addUser(@RequestBody User user) {
+		logger.info("user : " + user);
 		return userService.addUser(user);
 	}
 	
 	@GetMapping(value = "/getUserById/{id}")
 	public User getUserById(@PathVariable("id") Long id) {
-		System.out.println(id);
+		logger.info("id : " +id);
 		return userService.getUserById(id);
 	}
 	
 	@GetMapping("/getAllUsers")
 	public List<User> getAllUsers(){
+		logger.info(" getAllUsers called");
 		return userService.getAllUsers();
 	}
 	
 	@GetMapping("/getByEmail/{emailid}")
 	public User getUserByEmail(@PathVariable(value = "emailid") String emailid) {
+		logger.info("emailid : " + emailid);
 		return userService.getUserByEmail(emailid);
 	}
 }
