d28ffdcee23c7ff0e6e50797ba1d2bd03b672740
myjobsash <myjobsash@gmail.com>
2024-07-24 11:37:12 +0530

    Adding project from other directory

--- PRIVATECLOUD/eamapigateway/src/main/java/com/apigateway/EamapigatewayApplication.java
+++ PRIVATECLOUD/eamapigateway/src/main/java/com/apigateway/EamapigatewayApplication.java
@@ -1,16 +1,25 @@
 package com.apigateway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class EamapigatewayApplication {
+@CrossOrigin(origins = "http://localhost:4200")
+public class EamapigatewayApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EamapigatewayApplication.class, args);
 		System.setProperty("spring.config.name", "application");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("EamapigatewayApplication has been called......");
+		
 	}
 
 }
